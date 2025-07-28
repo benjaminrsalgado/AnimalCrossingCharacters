0c1ddbd0fdb19f7d32b418f440b1ffa40358970b
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-07-28 11:07:00 -0600

    Initial Commit

---
+++ Enviroment/EnviromentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnviromentApp.swift
+//  Enviroment
+//
+//  Created by Benjamin Rojo on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct EnviromentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
