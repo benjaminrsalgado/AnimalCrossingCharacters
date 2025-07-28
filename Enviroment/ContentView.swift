6b958b8e8fc5f6ebacdf22ce04426f2fbdbdd4b9
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-07-28 13:43:43 -0600

    i add images and foreach

--- Enviroment/ContentView.swift
+++ Enviroment/ContentView.swift
@@ -10,54 +10,56 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-           VistaPrincipal()
+            ListaDePersonajes()
             
         }
 
     }
 }
 
-class ViewModel: ObservableObject {
-    @Published var count = 0
-    
-    func plus(){
-        count += 1
-    }
+struct Personaje: Identifiable {
+    let id = UUID()
+    let nombre: String
+    let imagen: String
+    var esFavorito: Bool
 }
 
-struct VistaPrincipal: View{
-    @StateObject var copia = ViewModel()
+struct ListaDePersonajes: View{
+    @State private var personajes: [Personaje] = [
+        Personaje(nombre: "Taylor Swift", imagen: "taylor", esFavorito: false),
+        Personaje(nombre: "Travis Kelcey", imagen: "travis", esFavorito: false),
+        Personaje(nombre: "Sabrina carpenter", imagen: "sabrina", esFavorito: false)
+    ]
     var body: some View{
-        VStack{
-            VistaHija()
-            BotonView()
-               
-        }
-        .environmentObject(copia)
-    }
-}
+        ScrollView {
+            VStack {
+                ForEach(personajes.indices, id: \.self) { index in
+                    VStack {
+                        Image(personajes[index].imagen) // imagen local en Assets
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 120, height: 120)
 
-struct VistaHija: View{
-    @EnvironmentObject var copia : ViewModel
-    var body: some View{
-        Text("Contador actual: \(copia.count)")
-    }
-}
+                        Text(personajes[index].nombre)
+                            .font(.headline)
 
-struct BotonView:View{
-    @EnvironmentObject var copia : ViewModel
-    var body: some View{
-        VStack{
-            Button(action:{
-                copia.plus()
-            }){
-                Text("ahora es \(copia.count)")
+                        Button(action: {
+                            personajes[index].esFavorito.toggle() // cambia el favorito
+                        }) {
+                            Image(systemName: personajes[index].esFavorito ? "star.fill" : "star")
+                                .foregroundColor(.yellow)
+                                .font(.title)
+                        }
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(12)
+                    .padding(.bottom, 10)
+                }
             }
         }
+        }
     }
-
-}
-
 
 
 
